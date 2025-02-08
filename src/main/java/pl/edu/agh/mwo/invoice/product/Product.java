4fcdc4f4054e29e3eaca3601fcdd0c9643adb813
Piotrando1 <czernik.piotr@gmail.com>
2025-02-08 11:17:04 +0100

    changed Product class

--- src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
+++ src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
@@ -1,33 +1,50 @@
 package pl.edu.agh.mwo.invoice.product;
 
 import java.math.BigDecimal;
+import java.sql.SQLOutput;
 
 public abstract class Product {
+
     private final String name;
-
     private final BigDecimal price;
-
     private final BigDecimal taxPercent;
 
     protected Product(String name, BigDecimal price, BigDecimal tax) {
+
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("Product name cannot be null or empty.");
+        }
+
+        if (price == null) {
+            throw new IllegalArgumentException("price cant have 0 value");
+        }
+
+        if (price.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException("Price cannot be negative");
+        }
+
+        if (tax == null) {
+            throw new IllegalArgumentException("tax cant equal to 0");
+        }
+
         this.name = name;
         this.price = price;
         this.taxPercent = tax;
     }
 
     public String getName() {
-        return null;
+        return this.name;
     }
 
     public BigDecimal getPrice() {
-        return null;
+        return this.price;
     }
 
     public BigDecimal getTaxPercent() {
-        return null;
+        return this.taxPercent;
     }
 
     public BigDecimal getPriceWithTax() {
-        return null;
+        return this.price.multiply(this.getTaxPercent()).add(this.price);
     }
 }
